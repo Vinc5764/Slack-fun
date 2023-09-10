1b23ad2afb070e7e7b7b404dc0e1b13bbd7bcb52
vinny@charmswraith <vincentdanso05@gmail.com>
2023-09-10 22:13:24 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const todayDate = new Date();
+const currentDayOfWeek = weekday[todayDate.getDay()];
+document.getElementById('today').textContent = currentDayOfWeek;
+
+function refreshTime () {
+    const currentUTCTimeMilliseconds = Date.now();
+    const utcTime = document.getElementById('utctime');
+    document.getElementById('utctime').textContent = currentUTCTimeMilliseconds;
+    console.log(utcTime);
+}
+setInterval(refreshTime, 100);
